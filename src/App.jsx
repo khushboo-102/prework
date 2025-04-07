6ec5c9d75ae2c1dffd5669cc49b0020e1b5fabb7
Khushboo <khushbooatwork30@gmail.com>
2025-04-07 16:58:56 +0530

    created new MainSection2 component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
  import Navigation from "./Components/Navigation"; 
  import Herobar from "./Components/Herobar"
  import MainSection1 from "./Components/MainSection1"
+ import MainSection2 from "./Components/MainSection2"
+
 
 
  function App (){
@@ -11,6 +13,7 @@ return (
     <Navigation/>
     <Herobar/>
     <MainSection1/>
+    <MainSection2/>
     </>
 )} 
 
