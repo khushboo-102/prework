84ce424df7edc223b6319f3444bf7fe4eb30ff72
Khushboo <khushbooatwork30@gmail.com>
2025-04-08 14:38:18 +0530

    completed the MainSection3 component

---
+++ src/Components/MainSection3.jsx
@@ -0,0 +1,22 @@
+import "../Styles/MainSection3.css"
+function MainSection3({image, firstpara, heading, secondpara, lists, button}){
+    return(
+        <main >
+            <section className="section">
+                <img src={image} alt="" />
+                <p className="subtittle">{firstpara}</p>
+                <p>{heading}</p>
+                <p>{secondpara}</p>
+                <ul>
+                    {
+                        lists.map((item, index)=>(
+                            <li key={index}>{item}</li>
+                        ))
+                    }
+                </ul>
+                <button>{button}</button>
+              </section>
+        </main>
+    )
+}
+export default MainSection3
