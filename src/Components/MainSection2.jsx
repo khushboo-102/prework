1af2c743e7ec80ec2baf934b8275627d8c42b961
Khushboo <khushbooatwork30@gmail.com>
2025-04-09 12:49:11 +0530

    did some changes in mainsection2

--- src/Components/MainSection2.jsx
+++ src/Components/MainSection2.jsx
@@ -6,12 +6,12 @@ function MainSection2(){
            <p>Authorized retailer for</p> 
            <div className="partnerName">
             <p><img src="AT&T.png" alt=""/>AT&T</p>
-            <p><img src="kinetic.png" alt="" />kinetic</p>
-            <p>ziplefiber</p>
+            <p><img src="kinetic.png" alt=""/> <span>kinetic</span></p>
+            <p>ziple<span>fiber</span></p>
             <p><img src="earthLink.png" alt="" />EarthLink</p>
             <p><img src="AT&T.png" alt=""/>AT&T</p>
-            <p><img src="kinetic.png" alt="" />kinetic</p>
-            <p>ziplyfiber</p>
+            <p><img src="kinetic.png" alt=""/> <span>kinetic</span></p>
+            <p>ziply <span>fiber</span></p>
             <p><img src="earthLink.png" alt="" />EarthLink</p>
            </div>
          </main>
