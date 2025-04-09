7c364e18b14835604d518df947154dc137417792
Khushboo <khushbooatwork30@gmail.com>
2025-04-09 11:16:09 +0530

    do some changes in Mainsection2 component

--- src/Components/MainSection2.jsx
+++ src/Components/MainSection2.jsx
@@ -5,14 +5,14 @@ function MainSection2(){
         <main className="retailerDiv">
            <p>Authorized retailer for</p> 
            <div className="partnerName">
-            <p>AT&T</p>
-            <p>kinetic</p>
+            <p><img src="AT&T.png" alt=""/>AT&T</p>
+            <p><img src="kinetic.png" alt="" />kinetic</p>
             <p>ziplefiber</p>
-            <p>EarthLink</p>
-            <p>AT&T</p>
-            <p>kinetic</p>
+            <p><img src="earthLink.png" alt="" />EarthLink</p>
+            <p><img src="AT&T.png" alt=""/>AT&T</p>
+            <p><img src="kinetic.png" alt="" />kinetic</p>
             <p>ziplyfiber</p>
-            <p>EarthLink</p>
+            <p><img src="earthLink.png" alt="" />EarthLink</p>
            </div>
          </main>
     )
